ca13c23e267ebeb40b618ae71d1f470cb241e130
Hamzou <karouihamza@gmail.com>
2018-02-15 15:03:35 +0100

    ajout de flash message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,10 @@ def index
 end
 
 #def tweet qui appelle le service SendTweet en constituant les parametres tweet et message(à rentrer)
+#redirection à la home une fosi envoyé
 def tweet
  SendTweet.new(params[:tweet][:message]).perform
+ 	flash[:success] = "Tweet envoyé"
  redirect_to root_path
 end
 
